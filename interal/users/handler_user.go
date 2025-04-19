7357af6b2dc79827bc1d9eb8a24e6bc96be6f85e
Albina Mollander <albinamo@student.21-school.ru>
2025-04-19 20:38:08 +0300

    Структуры,ручки2

--- interal/users/handler_user.go
+++ interal/users/handler_user.go
@@ -3,6 +3,7 @@ package users
 import (
 	"avitotes/config"
 	"avitotes/interal/dto/payloadError"
+	"avitotes/pkg/req"
 	"log"
 	"net/http"
 )
@@ -11,6 +12,7 @@ type UserHandlerDependency struct {
 	*UserService
 	*config.Config
 }
+
 type UserHandler struct {
 	*UserService
 	*config.Config
@@ -28,14 +30,49 @@ func (userHandler *UserHandler) CreateUser() http.HandlerFunc {
 			log.Println("CreateUser: не удалось создать пользователя")
 			return
 		}
-		req.JsonResponse(&w, &createUser)
+		req.JsonResponse(&w, &createdUser)
 	}
 }
+
+func (userHandler *UserHandler) AuthenticateUser() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := req.HandleBody[payload.UserAuthRequest](&w, r)
+		if err != nil {
+			log.Println("AuthenticateUser: функция HandleBody вернула nil", err)
+			return
+		}
+		jwtPoint, err := userHandler.Login(body.Email, body.Password)
+		if err != nil {
+			log.Println("AuthenticateUser", err)
+			return
+		}
+		req.JsonResponse(&w, &jwtPoint)
+	}
+}
+
+func (UserHandler *UserHandler) GetTokenByRole() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := req.HandleBody[payload.TokenRequestRole](&w, r)
+		if err != nil {
+			log.Println("GetTokenByRole функция HandleBody вернула nill", err)
+			return
+		}
+		tokenStr, err := userHandler.GetToken((*body), Role)
+		if err != nil {
+			log.Println("GetTokenByRole", err)
+			return
+		}
+		req.JsonResponse(&w, &tokenStr)
+	}
+}
+
 func NewUserHandler(router *http.ServeMux, userHandlerDep *UserHandlerDependency) *UserHandler {
 	userHandler := &UserHandler{
 		userHandlerDep.UserService,
 		userHandlerDep.Config,
 	}
 	router.HandleFunc("POST /register", userHandler.CreateUser())
+	router.HandleFunc("POST /login", userHandler.AuthenticateUser())
+	router.HandleFunc("POST /dummyLogin", userHandler.GetTokenByRole())
 	return userHandler
 }
--- interal/users/repo_user.go
+++ interal/users/repo_user.go
@@ -1,1 +1,45 @@
 package users
+
+import (
+	"avitotes/database"
+	"fmt"
+	"log"
+)
+
+type UserRepoDeps struct {
+	Database *database.Db
+}
+
+type UserRepo struct {
+	Database *database.Db
+}
+
+func NewUserRepo(database *database.Db) *UserRepo {
+	return &UserRepo{
+		Database: database,
+	}
+}
+
+func (repo *UserRepo) Creat(user *User) (*User, error) {
+	query := `INSERT INTO users(email,password,role) VALUES ($1,&2,&3) RETURNING id`
+	err := repo.Database.MyDb.QueryRow(query, user.Email, user.Password, user.Role).Scan(&user.Id)
+	if err != nil {
+		log.Println("Create", err)
+		return nil, fmt.Errorf("Ошибка при создании пользователя: %w", err)
+	}
+	return user, nil
+}
+
+func (repo *UserRepo) FindUserByEmailPass(email, password string) (*User, error) {
+
+	//запрос
+	query := `SELECT id,email,password, role FROM users where email = $1 and password = $2`
+	//из бд дастаем данные и иницилизируем структуру
+	var user User
+	err := repo.Database.MyDb.QueryRow(query, email, password).Scan(&user.Id, &user.Email, &user.Password, &user.Role)
+	if err != nil {
+		log.Println("FindUserByEmailPass:", err)
+		return nil, fmt.Errorf("Нет пользователя с логином и паролем %s %s", email, password)
+	}
+	return &user, nil
+}
